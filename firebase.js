8a87bc2868d2e4895d8b14a22b2fd905b1e4cc30
Celinda Cueto <71739893+celycueto-ux@users.noreply.github.com>
2021-02-14 20:22:25 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJpLaOM8kenhRWXpJxrgsaCg71o5gLA08",
+  authDomain: "crud-react-bf732.firebaseapp.com",
+  databaseURL: "https://crud-react-bf732-default-rtdb.firebaseio.com",
+  projectId: "crud-react-bf732",
+  storageBucket: "crud-react-bf732.appspot.com",
+  messagingSenderId: "915817290538",
+  appId: "1:915817290538:web:fb3f7abc62fe07563cdc53",
+  measurementId: "G-D3RT3PZMMW"
+};
+
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+  
+ export default firebase;
